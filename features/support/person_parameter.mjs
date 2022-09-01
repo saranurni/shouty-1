648eb015ae3f45d5f32d64969872e21795be5732
Sara Nurestani <saranurestani@gmail.com>
2022-09-01 23:09:48 +0100

    Remove uneccessary files

--- features/support/person_parameter.mjs
+++
@@ -1,8 +0,0 @@
-import { defineParameterType } from "@cucumber/cucumber";
-
-["person", "listener", "shouter"].forEach((type) => {
-  defineParameterType({
-    name: type,
-    regexp: /S[a-zA-Z]*|L[a-zA-Z]*/,
-  });
-});
